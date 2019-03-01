358095e0fc6c79221d406c2b6829969e15eb55f6
unknown <shakshigarg9416858875@gmail.com>
2019-03-02 00:52:38 +0530

    updated apis

--- functions/index.js
+++ functions/index.js
@@ -3,20 +3,58 @@ const admin=require('firebase-admin');
 const jwt = require('jsonwebtoken');
 const crypto=require('crypto');
 const jsonpatch=require('jsonpatch');
+const cors = require('cors');
+const express = require('express');
 admin.initializeApp();
 const database=admin.database();
+var Jimp=require('jimp');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(cors({origin:true}))
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(function(req,res,next) {
 
+    res.header("Access-Control-Allow-Origin","*")
+    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+    next()
+})
+
+
+app.use('/login',login)
+app.use('/patching',authenticate,patching)
+app.use('/register',register)
+app.use('/generateThumbnail',authenticate,generateThumbnail)
+
+function authenticate(request,response,next)
+{
+    if(request.body.accessToken === undefined || request.body.accessToken === '')
+    {
+        return response.json({error : true,location : "empty or undefined access token"})
+    }
+
+    let accessToken = request.body.accessToken;
+    jwt.verify(accessToken,'RSG',(err,data)=>{
+        if(err !== null)
+        {
+            return response.json({error : err,location : "falied to verify"})
+        }
+        else
+        {         
+            return next();
+        }
+    })
+}
 
 function loginBody(request,response)
 {
-  let username=request.body.username;
+    let username=request.body.username;
     let password=request.body.password;
     password= crypto.createHash('md5').update(password).digest('hex');
     let ref=database.ref();
     ref.once('value',function(snapshot){
     if(!snapshot.hasChild(`/${username}`)) {
         let err={
-              error: "username does not exist"
+              bit: "1"
             }
         return response.json(err);
     }
@@ -30,78 +68,86 @@ function loginBody(request,response)
                     username: username
                   }
                   const token=jwt.sign(data,'RSG',{ expiresIn:"12h"});
-                  return response.json(token);
+                  let reply={
+                    accessToken: token,
+                    bit: "3"
+                  }
+              return response.json(reply);
             }
             let err1={
-                     error: "password is invalid"
-                }
+                     bit: "2"
+            }
             return response.json(err1);
       })
     });
 }
 
-exports.login=functions.https.onRequest(function(request,response){
+function login(request,response){
     loginBody(request,response);
-})
+}
 
-exports.pathcing=functions.https.onRequest(function(request,response){
-    let jsonObject=request.body.jsonObject;
-    let jsonPatchObject=request.body.jsonPatchObject;
+function patching(request,response){
+    let jsonObject=JSON.stringify(request.body.jsonObject);
+    let jsonPatchObject=JSON.stringify(request.body.jsonPatchObject);
     patcheddoc = jsonpatch.apply_patch(jsonObject, jsonPatchObject);  
     console.log(patcheddoc);
     if(patcheddoc)
     {
-        return response.json(patcheddoc);
+        return response.json({
+          bit: "2",
+          patcheddoc: patcheddoc
+        });
     }
     else
     {
       return response.json({
-        message: "error"
+        message: "error",
+        bit : "1"
       })
     }
-})
+}
 
-exports.register = functions.https.onRequest((request, response) => {
-  let username=request.body.username;
+function register(request, response){  
+    let username=request.body.username;
     let password=request.body.password;
     let pass=crypto.createHash('md5').update(password).digest('hex');
     let path=`/${username}/`
     const ref=database.ref();
-
     ref.once('value',(snapshot)=> {
-        if (snapshot.hasChild(path)) {
+        if(snapshot.hasChild(path)) {
             return response.json({
-              message:"user already registered! please login to continue",
+              bit: "4",
+              message:"user already registered! please login to continue"
             });
         }
-       }).then((snap)=>{
+    }).then((snap)=>{
          ref.child(path).set({
-      password : pass
+            password : pass
     })
         return response.json(loginBody(request,response));
   })
          .catch((err)=>{
-          return response.json({
-          error : err
+            return response.json({
+              error : err
           })
          })
-});
+}
 
-var Jimp=require('jimp');
-exports.th=functions.https.onRequest(function(request,response){
-Jimp.read('https://www.w3schools.com/images/w3schools_green.jpg')
+
+function generateThumbnail(request,response){
+Jimp.read(request.body.imageURL)
   .then(image => {
-    console.log("hi")
-    console.log(image)
     return response.json(image
       .resize(50, 50) // resize
       .quality(60) // set JPEG quality
       .greyscale() // set greyscale
-      .write('C:/Users/Lenovo/Pictures/img/1.jpg'));
+      .write('D:/img.jpg'));
   })
   .catch(err => {
     console.log("here")
+    console.log(err);
     return response.json(err);
   });
-  
-})
+}
+
+exports.api = functions.https.onRequest(app);
